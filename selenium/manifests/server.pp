c75b269fbd494d880bf51f4d0b9e4c103f52f0b7
Adam Goucher <adam@goucher.ca>
2011-03-17 01:17:52 -0400

    check for the service to be up as well

--- selenium/manifests/server.pp
+++ selenium/manifests/server.pp
@@ -1,9 +1,18 @@
 class selenium-server {
-	file { "/usr/local/selenium":
-		ensure => directory,
-	}
-	file { "/usr/local/selenium/selenium-server-standalone-2.0b2.jar":
-		ensure => present,
-		source => "puppet:///modules/selenium/selenium-server-standalone-2.0b2.jar"
-	}
+        file { "/usr/local/selenium":
+                ensure => directory,
+
+                "/usr/local/selenium/selenium-server-standalone-2.0b2.jar":
+                ensure => present,
+                source => "puppet:///modules/selenium/selenium-server-standalone-2.0b2.jar";
+
+                "/etc/init.d/selenium-server":
+                ensure => present,
+                source => "puppet:///modules/selenium/selenium-server",
+                mode => 0755
+        }
+        service { "selenium-server":
+                ensure => running,
+                pattern => "selenium-server-standalone-2.0b2.jar"
+        }
 }
