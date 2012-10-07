47e62acf80d7282f684f722585fbde7d6066023e
adamgoucher <adam@goucher.ca>
2012-10-06 19:50:07 -0700

    starting the selenium server on windows

--- selenium/manifests/server.pp
+++ selenium/manifests/server.pp
@@ -1,13 +1,20 @@
 class selenium::server {
   if $operatingsystem == 'windows' {
-    file {"c:\\selenium":
+    file { "c:/selenium":
       ensure => directory,
     }
 
-    file { "c:\\selenium\\selenium-server-standalone-2.25.0.jar":
+    file { "c:/selenium/selenium-server-standalone-2.25.0.jar":
       ensure => present,
-      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar";
+      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar",
     }
+
+    exec { 'start server':
+      path => $path,
+      command => "cmd.exe /c start cmd /k \"\"${jre7_home}\\bin\\java.exe\" -jar c:\\selenium\\selenium-server-standalone-2.25.0.jar\"",
+      require => File["c:/selenium/selenium-server-standalone-2.25.0.jar"],
+    }
+
 
   } else {
     file {"/usr/local/selenium":
@@ -16,18 +23,18 @@ class selenium::server {
 
     file {"/usr/local/selenium/selenium-server-standalone-2.25.0.jar":
       ensure => present,
-      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar";
+      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar",
     }
 
     file {"/etc/init.d/selenium-server":
       ensure => present,
       source => "puppet:///modules/selenium/selenium-server",
-      mode => 0755
+      mode => 0755,
     }
 
     service { "selenium-server":
       ensure => running,
-      pattern => "selenium-server-standalone-2.0b2.jar"
+      pattern => "selenium-server-standalone-2.0b2.jar",
     }
   }
 }
