768d051f9acc4b5fe53e9b2d6bf1d67b36a904b9
adamgoucher <adam@goucher.ca>
2012-10-06 17:28:46 -0700

    server jar copies down to windows

--- selenium/manifests/server.pp
+++ selenium/manifests/server.pp
@@ -1,18 +1,33 @@
-class selenium-server {
-        file { "/usr/local/selenium":
-                ensure => directory,
+class selenium::server {
+  if $operatingsystem == 'windows' {
+    file {"c:\\selenium":
+      ensure => directory,
+    }
 
-                "/usr/local/selenium/selenium-server-standalone-2.0b2.jar":
-                ensure => present,
-                source => "puppet:///modules/selenium/selenium-server-standalone-2.0b2.jar";
+    file { "c:\\selenium\\selenium-server-standalone-2.25.0.jar":
+      ensure => present,
+      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar";
+    }
 
-                "/etc/init.d/selenium-server":
-                ensure => present,
-                source => "puppet:///modules/selenium/selenium-server",
-                mode => 0755
-        }
-        service { "selenium-server":
-                ensure => running,
-                pattern => "selenium-server-standalone-2.0b2.jar"
-        }
+  } else {
+    file {"/usr/local/selenium":
+      ensure => directory,
+    }
+
+    file {"/usr/local/selenium/selenium-server-standalone-2.25.0.jar":
+      ensure => present,
+      source => "puppet:///modules/selenium/selenium-server-standalone-2.25.0.jar";
+    }
+
+    file {"/etc/init.d/selenium-server":
+      ensure => present,
+      source => "puppet:///modules/selenium/selenium-server",
+      mode => 0755
+    }
+
+    service { "selenium-server":
+      ensure => running,
+      pattern => "selenium-server-standalone-2.0b2.jar"
+    }
+  }
 }
