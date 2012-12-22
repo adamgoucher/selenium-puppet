3f82bb72dbf507f54ca2ac80280e345c08870c9d
Adam Goucher <adam@goucher.ca>
2012-12-21 23:27:54 -0500

    more puppet

--- selenium/manifests/user.pp
+++ selenium/manifests/user.pp
@@ -1,36 +1,37 @@
+# Installs a user to run the Selenium server as
 class selenium::user {
-  if $operatingsystem == 'windows' {
-    file { "${profiles_directory}/selenium":
+  if $::operatingsystem == 'windows' {
+    file { "${::profiles_directory}/selenium":
       ensure => directory
     }
 
-    file { "${profiles_directory}/selenium/NTUSER.dat":
+    file { "${::profiles_directory}/selenium/NTUSER.dat":
       ensure => present,
-      source => "${profiles_directory}/Default User/NTUSER.dat",
+      source => "${::profiles_directory}/Default User/NTUSER.dat",
     }
 
-    user { selenium-user:
-      comment => "user for selenium scripts",
-      password => "selenium",
-      groups => ["Administrators"],
+    user { 'selenium-user':
+      comment   => 'user for selenium scripts',
+      password  => 'selenium',
+      groups    => ['Administrators'],
     }
 
     registry::value { 'autologin-username':
-      key => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
-      value => "DefaultUserName",
-      data => "selenium-user"
+      key   => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
+      value => 'DefaultUserName',
+      data  => 'selenium-user'
     }
 
     registry::value { 'autologin-password':
-      key => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
-      value => "DefaultPassword",
-      data => "selenium"
+      key   => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
+      value => 'DefaultPassword',
+      data  => 'selenium'
     }
 
     registry::value { 'autologin-enable':
-      key => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
-      value => "AutoAdminLogon",
-      data => "1"
+      key   => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
+      value => 'AutoAdminLogon',
+      data  => '1'
     }
-  } 
+  }
 }
