a2757e4269edda4a14489dc28c1a4da40514f5ce
adamgoucher <adam@goucher.ca>
2012-12-26 05:31:54 -0800

    fixing the windows version factor

--- jre7/lib/facter/jre_version.rb
+++ jre7/lib/facter/jre_version.rb
@@ -1,8 +1,10 @@
 require 'facter'
-# require 'facter/util/registry'
 
 Facter.add(:jre7_version) do
   confine :kernel => :windows
+  
+  require 'facter/util/registry'
+
   setcode do
     version = Facter::Util::Registry.hklm_read('SOFTWARE\JavaSoft\Java Runtime Environment', 'Java7FamilyVersion')
     version ||= nil
