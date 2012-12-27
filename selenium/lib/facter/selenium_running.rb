dbb8c1067565f603f07f170714dbe0062048afd4
adamgoucher <adam@goucher.ca>
2012-12-27 14:04:14 -0800

    facts are read as strings, so returning false wasnt enough to be able to do bools on it. just returning nil now if not running

--- selenium/lib/facter/selenium_running.rb
+++ selenium/lib/facter/selenium_running.rb
@@ -3,7 +3,6 @@ require 'facter'
 Facter.add(:selenium_server_running) do
   confine :kernel => :windows
   setcode do
-    running = false
     tasks = Facter::Util::Resolution.exec('tasklist.exe /V /FO csv')
     tasks.each do |task|
       if task.match('.*-jar c:\\\selenium\\\selenium-server-standalone.*.jar"$')
@@ -11,6 +10,6 @@ Facter.add(:selenium_server_running) do
         break
       end
     end
-    running
+    running ||= nil
   end
 end
