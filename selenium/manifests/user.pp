7707501a249b397e7ab485d4c93eef56cea0305a
Adam Goucher <adam@goucher.ca>
2012-12-28 13:26:39 -0500

    first crack at hiera

--- selenium/manifests/user.pp
+++ selenium/manifests/user.pp
@@ -1,31 +1,34 @@
 # Installs a user to run the Selenium server as
 class selenium::user {
+  $::selenium_user_username      = hiera('selenium_user_username', 'selenium_user')
+  $::selenium_user_username_password  = hiera('selenium_user_password', 'selenium')
+  
   if $::operatingsystem == 'windows' {
-    file { "${::profiles_directory}/selenium":
+    file { "${::profiles_directory}/${::selenium-user}":
       ensure => directory
     }
 
-    file { "${::profiles_directory}/selenium/NTUSER.dat":
+    file { "${::profiles_directory}/${::selenium-user}/NTUSER.dat":
       ensure => present,
       source => "${::profiles_directory}/Default User/NTUSER.dat",
     }
 
-    user { 'selenium-user':
+    user { $::selenium_user_username:
       comment   => 'user for selenium scripts',
-      password  => 'selenium',
+      password  => ${::selenium_user_password},
       groups    => ['Administrators'],
     }
 
     registry::value { 'autologin-username':
       key   => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
       value => 'DefaultUserName',
-      data  => 'selenium-user'
+      data  => $::selenium_user_username
     }
 
     registry::value { 'autologin-password':
       key   => 'HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon',
       value => 'DefaultPassword',
-      data  => 'selenium'
+      data  => $::selenium_user_username_password
     }
 
     registry::value { 'autologin-enable':
