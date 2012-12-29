c2e7e25490bb893e85bd1f536ad5a5d8b8eca58a
Adam Goucher <adam@goucher.ca>
2012-12-29 12:26:04 -0500

    playing with extending providers

---
+++ selenium/lib/puppet/provider/extended_windows_adsi.rb
@@ -0,0 +1,20 @@
+require 'puppet/util/adsi'
+
+Puppet::Type.type(:user).provide :extended_windows_adsi, :parent => :windows_adsi, :source => :windows_adsi do
+  desc "Local user management for Windows -- with some extra goodies."
+
+  confine    :operatingsystem => :windows
+  
+  def screensaver_enabled
+    v = `reg query "#{user.uid}\\control panel\\desktop" /v screensaveactive`
+    v.match(/screensaveactive\s*\w*\s*(\S)/).captures
+  end
+
+  def screensaver_enabled=(value)
+    if @resource.screensaver_enabled?
+      `reg add "hku\\#{user.uid}\\control panel\\desktop" /v screensaveactive /f /t REG_SZ /d 1`
+    else
+      `reg add "hku\\#{user.uid}\\control panel\\desktop" /v screensaveactive /f /t REG_SZ /d 0`
+    end
+  end
+end
